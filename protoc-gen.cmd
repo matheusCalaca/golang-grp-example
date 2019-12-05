5a248127777f63db7d46e5d23968f412e7340630
matheus Calaça <matheusfcalaca@gmail.com>
2019-12-04 21:10:39 -0300

    refatorando estrutura

--- protoc-gen.cmd
+++ protoc-gen.cmd
@@ -1,2 +1,2 @@
-protoc --proto_path=api/proto/pessoa --proto_path=third_party --go_out=plugins=grpc:pkg/api/pessoa endereco-service.proto
-protoc --proto_path=api/proto/pessoa --proto_path=third_party --go_out=plugins=grpc:pkg/api/pessoa pessoa-service.proto
+protoc --proto_path=app/interface/rpc/proto/pessoa --proto_path=third_party --go_out=plugins=grpc:app/interface/rpc/api/pessoa endereco-service.proto
+protoc --proto_path=app/interface/rpc/proto/pessoa --proto_path=third_party --go_out=plugins=grpc:app/interface/rpc/api/pessoa pessoa-service.proto
